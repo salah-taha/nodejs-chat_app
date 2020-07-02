0f4220b0783b40c259254f8b27b59ffa7c89b5cf
salah taha <salahtaha065@gmail.com>
2020-07-02 06:09:16 +0200

    working on home page

---
+++ controllers/home.js
@@ -0,0 +1,26 @@
+module.exports = function (async, Club, _) {
+  return {
+    SetRouting: function (router) {
+      router.get("/home", this.homePage);
+    },
+    homePage: function (req, res) {
+      async.parallel(
+        [
+          function (callback) {
+            Club.find({}, (err, result) => {
+              callback(err, result);
+            });
+          },
+        ],
+        (err, results) => {
+          const res1 = results[0];
+          console.log(res1);
+          return res.render("home", {
+            title: "Footballkik - salah",
+            data: res1,
+          });
+        }
+      );
+    },
+  };
+};
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const passport = require("passport");
 
 const container = require("./container");
 
-container.resolve(function (users, _, admin) {
+container.resolve(function (users, _, admin, home) {
   mongoose.set("useFindAndModify", false);
   mongoose.set("useCreateIndex", true);
   mongoose.Promise = global.Promise;
@@ -34,6 +34,7 @@ container.resolve(function (users, _, admin) {
     const router = require("express-promise-router")();
     users.SetRouting(router);
     admin.SetRouting(router);
+    home.SetRouting(router);
 
     app.use(router);
   }
