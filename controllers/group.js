ccbf3b1439f131be7d353b6fb0d3b8883127cbc8
salah taha <salahtaha065@gmail.com>
2020-07-02 22:42:00 +0200

    working on group page and socket io

---
+++ controllers/group.js
@@ -0,0 +1,14 @@
+module.exports = function (Club) {
+  return {
+    SetRouting: function (router) {
+      router.get("/group/:name", this.groupPage);
+    },
+    groupPage: function (req, res) {
+      const name = req.params.name;
+      res.render("groupchat/group", {
+        title: "Footballkik - Group",
+        name: name,
+      });
+    },
+  };
+};
