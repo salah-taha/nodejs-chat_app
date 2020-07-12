8b00b763ce4dd395ee975028fc3027518b75efbd
salah taha <salahtaha065@gmail.com>
2020-07-12 11:53:10 +0200

    finish add to favourite

--- controllers/home.js
+++ controllers/home.js
@@ -4,6 +4,7 @@ module.exports = function (async, Club, _, Users) {
   return {
     SetRouting: function (router) {
       router.get("/home", this.homePage);
+      router.post("/home", this.postHomePage);
     },
     homePage: function (req, res) {
       async.parallel(
@@ -56,6 +57,34 @@ module.exports = function (async, Club, _, Users) {
             countries: sortedCountries,
             user: req.user,
           });
+        }
+      );
+    },
+    postHomePage: function (req, res) {
+      async.parallel(
+        [
+          function (callback) {
+            Club.updateOne(
+              {
+                _id: req.body.id,
+                "fans.username": { $ne: req.user.username },
+              },
+              {
+                $push: {
+                  fans: {
+                    username: req.user.username,
+                    email: req.user.email,
+                  },
+                },
+              },
+              (err, count) => {
+                callback(err, count);
+              }
+            );
+          },
+        ],
+        (err, results) => {
+          res.redirect("/home");
         }
       );
     },
